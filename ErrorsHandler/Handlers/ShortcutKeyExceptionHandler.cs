1302ef6868d97426ad045787e3ee902ece1d0d4d
Raul Breje <brejeraul@gmail.com>
2016-06-17 23:15:25 +0300

    Error Handler implemented.

---
+++ ErrorsHandler/Handlers/ShortcutKeyExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Handlers
+{
+    class ShortcutKeyExceptionHandler : IExceptionHandler
+    {
+
+        private readonly IExceptionHandler successor;
+
+        public ShortcutKeyExceptionHandler(IExceptionHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public void Handle(Exception exception)
+        {
+            var testException = exception.InnerException<InvalidOperationException>();
+            successor.Handle(exception);
+        }
+    }
+}
---
+++ ErrorsHandler/Handlers/WordAudioExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Handlers
+{
+    class WordAudioExceptionHandler : IExceptionHandler
+    {
+
+        private readonly IExceptionHandler successor;
+
+        public WordAudioExceptionHandler(IExceptionHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public void Handle(Exception exception)
+        {
+            successor.Handle(exception);
+        }
+    }
+}
